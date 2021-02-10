418bc7db85406850501fa43edc29c0d34d6bf384
lk2322 <lk2322@ya.ru>
2021-02-10 19:11:58 +0500

    update

--- logic.py
+++ logic.py
@@ -2,25 +2,31 @@ from typing import Callable, Tuple
 
 
 class Logic():
-    def __init__(cords=(37.620070, 55.753630), zoom_level=10, map_type='map', render_callback: Callable):
+    def __init__(self, render_callback: Callable, cords=(37.620070, 55.753630), zoom_level=0.01, map_type='map'):
         self.cords = cords
         self.zoom_level = zoom_level
         self.map_type = map_type
         self.render_map = render_callback
         self.step = 0.15
 
-    @render_callback
     def move(self, dirc: Tuple[int, int]):
-        x = cords[0] += dirc[0] * (self.zoom_level // 100) * self.step
-        y = cords[1] += dirc[1] * (self.zoom_level // 100) * self.step
+        x = self.cords[0] + dirc[0] * self.step * self.zoom_level
+        y = self.cords[1] + dirc[1] * self.step * self.zoom_level
+        self.cords = (x, y)
+        self.render_map()
 
-    @render_callback
-    def change_zoom(self, change: int):
-        if 0 <= self.zoom_level + change <= 17:
-            self.zoom_level += change
+    def change_zoom(self, change: float):
+        if 0 > self.zoom_level + change:
+            return
+        self.zoom_level += change
+        self.render_map()
 
-    @render_callback
     def change_map(self, map_t: str):
-        if map_t not in ['map', 'sat', 'sat, skl']:
+        if map_t not in ['map', 'sat', 'sat,skl']:
             raise NameError("""Only 'map', 'sat', 'sat, skl' """)
         self.map_type = map_t
+        self.render_map()
+
+    def change_cords(self, cords):
+        self.cords = cords
+        self.render_map()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
+import logic
+import api
 from map_api import Ui_Form
 
 
@@ -7,8 +9,55 @@ class MapApp():
     def __init__(self):
         self.app = QtWidgets.QApplication(sys.argv)
         self.ui = Ui_Form(QtWidgets.QWidget())
+        self.ui.form.keyPressEvent = self.keyPressEvent
+        self.ui.form.mousePressEvent = self.mousePressEvent
         self.ui.form.show()
+        self.logic = logic.Logic(render_callback=self.render_map)
+        self.render_map()
+        self.ui.button_search.clicked.connect(self.change_cords)
+        self.ui.coords_y_lineedit.returnPressed.connect(self.ui.coords_y_lineedit.clearFocus)
+        self.ui.coords_x_lineedit.returnPressed.connect(self.ui.coords_x_lineedit.clearFocus)
+
         sys.exit(self.app.exec_())
+
+    def render_map(self):
+        pixmap = QtGui.QPixmap()
+        pixmap.loadFromData(api.get_map(self.logic.cords, self.logic.zoom_level, self.logic.map_type).read())
+        self.ui.map_label.setPixmap(pixmap)
+
+    def mousePressEvent(self, event):
+        focused_widget = QtWidgets.QApplication.focusWidget()
+        if isinstance(focused_widget, QtWidgets.QLineEdit):
+            focused_widget.clearFocus()
+
+    def change_cords(self):
+        x = float(self.ui.coords_x_lineedit.text())
+        y = float(self.ui.coords_y_lineedit.text())
+        self.logic.change_cords((x, y))
+        self.ui.coords_x_lineedit.clearFocus()
+        self.ui.coords_y_lineedit.clearFocus()
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Up:
+            self.logic.move((0, 1))
+        if event.key() == QtCore.Qt.Key_Down:
+            self.logic.move((0, -1))
+        if event.key() == QtCore.Qt.Key_Right:
+            self.logic.move((1, 0))
+        if event.key() == QtCore.Qt.Key_Left:
+            self.logic.move((-1, 0))
+        if event.key() == QtCore.Qt.Key_PageUp:
+            self.logic.change_zoom(0.05)
+        if event.key() == QtCore.Qt.Key_PageDown:
+            self.logic.change_zoom(-0.05)
+        if event.key() == QtCore.Qt.Key_1:
+            self.logic.change_map('map')
+        if event.key() == QtCore.Qt.Key_2:
+            self.logic.change_map('sat')
+        if event.key() == QtCore.Qt.Key_3:
+            self.logic.change_map('sat,skl')
+
+        event.accept()
 
 
 if __name__ == "__main__":
